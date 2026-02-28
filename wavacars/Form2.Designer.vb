60b6b091a7d6afb9b684991bf583b20e7e2519a7
willairwayvirtual <40953328+willairwayvirtual@users.noreply.github.com>
2026-02-28 18:02:16 +0000

    fwedksuhjpifdkhgdfo;k
    gewdsvc'xgklreh/gdc'ok;odfjjieg;hdbvok

--- wavacars/Form2.Designer.vb
+++ wavacars/Form2.Designer.vb
@@ -53,17 +53,19 @@ Partial Class screen2
         Me.Panel1.Controls.Add(Me.Label8)
         Me.Panel1.Controls.Add(Me.Label7)
         Me.Panel1.Dock = System.Windows.Forms.DockStyle.Bottom
-        Me.Panel1.Location = New System.Drawing.Point(0, 358)
+        Me.Panel1.Location = New System.Drawing.Point(0, 531)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(1306, 45)
+        Me.Panel1.Size = New System.Drawing.Size(1794, 55)
         Me.Panel1.TabIndex = 0
         '
         'PictureBox1
         '
         Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
-        Me.PictureBox1.Location = New System.Drawing.Point(1190, 0)
+        Me.PictureBox1.Location = New System.Drawing.Point(1587, 0)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(116, 50)
+        Me.PictureBox1.Size = New System.Drawing.Size(155, 62)
         Me.PictureBox1.TabIndex = 3
         Me.PictureBox1.TabStop = False
         '
@@ -71,9 +73,10 @@ Partial Class screen2
         '
         Me.Label8.AutoSize = True
         Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label8.Location = New System.Drawing.Point(348, 3)
+        Me.Label8.Location = New System.Drawing.Point(464, 4)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(589, 39)
+        Me.Label8.Size = New System.Drawing.Size(743, 52)
         Me.Label8.TabIndex = 2
         Me.Label8.Text = "you are connect to  fsx/ p3d / XPlane " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
         '
@@ -81,9 +84,10 @@ Partial Class screen2
         '
         Me.Label7.AutoSize = True
         Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label7.Location = New System.Drawing.Point(3, 3)
+        Me.Label7.Location = New System.Drawing.Point(4, 4)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(164, 33)
+        Me.Label7.Size = New System.Drawing.Size(207, 42)
         Me.Label7.TabIndex = 1
         Me.Label7.Text = "simconnect"
         '
@@ -92,9 +96,10 @@ Partial Class screen2
         Me.Label6.AutoSize = True
         Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label6.ForeColor = System.Drawing.Color.Red
-        Me.Label6.Location = New System.Drawing.Point(268, 274)
+        Me.Label6.Location = New System.Drawing.Point(409, 418)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(366, 31)
+        Me.Label6.Size = New System.Drawing.Size(458, 39)
         Me.Label6.TabIndex = 42
         Me.Label6.Text = "Enten Username before save"
         '
@@ -102,9 +107,10 @@ Partial Class screen2
         '
         Me.Label5.AutoSize = True
         Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label5.Location = New System.Drawing.Point(275, 229)
+        Me.Label5.Location = New System.Drawing.Point(419, 363)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(90, 29)
+        Me.Label5.Size = New System.Drawing.Size(109, 36)
         Me.Label5.TabIndex = 41
         Me.Label5.Text = "Uname"
         '
@@ -113,18 +119,20 @@ Partial Class screen2
         Me.Uname.BackColor = System.Drawing.Color.Silver
         Me.Uname.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Uname.ForeColor = System.Drawing.Color.White
-        Me.Uname.Location = New System.Drawing.Point(381, 230)
+        Me.Uname.Location = New System.Drawing.Point(560, 364)
+        Me.Uname.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Uname.Name = "Uname"
-        Me.Uname.Size = New System.Drawing.Size(215, 38)
+        Me.Uname.Size = New System.Drawing.Size(285, 46)
         Me.Uname.TabIndex = 40
         '
         'Button2
         '
         Me.Button2.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
         Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button2.Location = New System.Drawing.Point(895, -6)
+        Me.Button2.Location = New System.Drawing.Point(1193, -7)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(424, 71)
+        Me.Button2.Size = New System.Drawing.Size(565, 87)
         Me.Button2.TabIndex = 39
         Me.Button2.Text = "save myHours"
         Me.Button2.UseVisualStyleBackColor = False
@@ -134,9 +142,10 @@ Partial Class screen2
         Me.lblTimer.AutoSize = True
         Me.lblTimer.BackColor = System.Drawing.Color.DarkSeaGreen
         Me.lblTimer.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblTimer.Location = New System.Drawing.Point(389, 77)
+        Me.lblTimer.Location = New System.Drawing.Point(571, 176)
+        Me.lblTimer.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lblTimer.Name = "lblTimer"
-        Me.lblTimer.Size = New System.Drawing.Size(176, 73)
+        Me.lblTimer.Size = New System.Drawing.Size(215, 91)
         Me.lblTimer.TabIndex = 37
         Me.lblTimer.Text = "0:0:0"
         '
@@ -145,9 +154,10 @@ Partial Class screen2
         Me.Label71.AutoSize = True
         Me.Label71.BackColor = System.Drawing.Color.Red
         Me.Label71.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label71.Location = New System.Drawing.Point(213, 155)
+        Me.Label71.Location = New System.Drawing.Point(336, 272)
+        Me.Label71.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label71.Name = "Label71"
-        Me.Label71.Size = New System.Drawing.Size(555, 20)
+        Me.Label71.Size = New System.Drawing.Size(681, 25)
         Me.Label71.TabIndex = 36
         Me.Label71.Text = "Please put parking brake onAnd cut off your engine(s) before starting ACARS"
         '
@@ -155,9 +165,10 @@ Partial Class screen2
         '
         Me.CompFlight.BackColor = System.Drawing.Color.Red
         Me.CompFlight.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.CompFlight.Location = New System.Drawing.Point(530, 178)
+        Me.CompFlight.Location = New System.Drawing.Point(759, 300)
+        Me.CompFlight.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.CompFlight.Name = "CompFlight"
-        Me.CompFlight.Size = New System.Drawing.Size(257, 46)
+        Me.CompFlight.Size = New System.Drawing.Size(343, 57)
         Me.CompFlight.TabIndex = 35
         Me.CompFlight.Text = "Complete Flight"
         Me.CompFlight.UseVisualStyleBackColor = False
@@ -166,9 +177,10 @@ Partial Class screen2
         '
         Me.StFlight.BackColor = System.Drawing.Color.LightSalmon
         Me.StFlight.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.StFlight.Location = New System.Drawing.Point(125, 173)
+        Me.StFlight.Location = New System.Drawing.Point(219, 294)
+        Me.StFlight.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.StFlight.Name = "StFlight"
-        Me.StFlight.Size = New System.Drawing.Size(343, 51)
+        Me.StFlight.Size = New System.Drawing.Size(457, 63)
         Me.StFlight.TabIndex = 34
         Me.StFlight.Text = "start  Flight"
         Me.StFlight.UseVisualStyleBackColor = False
@@ -178,9 +190,10 @@ Partial Class screen2
         Me.TextBox2.BackColor = System.Drawing.Color.Silver
         Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.TextBox2.ForeColor = System.Drawing.Color.White
-        Me.TextBox2.Location = New System.Drawing.Point(653, 2)
+        Me.TextBox2.Location = New System.Drawing.Point(871, 2)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(246, 38)
+        Me.TextBox2.Size = New System.Drawing.Size(327, 46)
         Me.TextBox2.TabIndex = 29
         '
         'TextBox1
@@ -188,9 +201,10 @@ Partial Class screen2
         Me.TextBox1.BackColor = System.Drawing.Color.Silver
         Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.TextBox1.ForeColor = System.Drawing.Color.White
-        Me.TextBox1.Location = New System.Drawing.Point(208, 6)
+        Me.TextBox1.Location = New System.Drawing.Point(277, 7)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(240, 38)
+        Me.TextBox1.Size = New System.Drawing.Size(319, 46)
         Me.TextBox1.TabIndex = 28
         '
         'Timer1
@@ -198,18 +212,20 @@ Partial Class screen2
         '
         'ProgressBar1
         '
-        Me.ProgressBar1.Location = New System.Drawing.Point(1, 308)
+        Me.ProgressBar1.Location = New System.Drawing.Point(53, 460)
+        Me.ProgressBar1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.ProgressBar1.Name = "ProgressBar1"
-        Me.ProgressBar1.Size = New System.Drawing.Size(1305, 51)
+        Me.ProgressBar1.Size = New System.Drawing.Size(1740, 63)
         Me.ProgressBar1.TabIndex = 43
         '
         'Fltnum
         '
         Me.Fltnum.AutoSize = True
         Me.Fltnum.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Fltnum.Location = New System.Drawing.Point(50, 9)
+        Me.Fltnum.Location = New System.Drawing.Point(67, 11)
+        Me.Fltnum.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Fltnum.Name = "Fltnum"
-        Me.Fltnum.Size = New System.Drawing.Size(97, 31)
+        Me.Fltnum.Size = New System.Drawing.Size(121, 39)
         Me.Fltnum.TabIndex = 44
         Me.Fltnum.Text = "Fltnum"
         '
@@ -217,17 +233,18 @@ Partial Class screen2
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(491, 9)
+        Me.Label1.Location = New System.Drawing.Point(655, 11)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(105, 31)
+        Me.Label1.Size = New System.Drawing.Size(131, 39)
         Me.Label1.TabIndex = 45
         Me.Label1.Text = "DeptAir"
         '
         'screen2
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1306, 403)
+        Me.ClientSize = New System.Drawing.Size(1794, 586)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Fltnum)
         Me.Controls.Add(Me.ProgressBar1)
@@ -244,6 +261,7 @@ Partial Class screen2
         Me.Controls.Add(Me.Panel1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "screen2"
         Me.Text = "screen2"
         Me.Panel1.ResumeLayout(False)
